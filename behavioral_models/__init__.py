bdf73a3a837a9f9ade7e45c38738e216728ab09b
Leonardo Gargitter <leogargitter@gmail.com>
2023-01-24 17:32:33 -0300

    Implementação design pattern 'Strategy'
    Implementada a design pattern 'strategy' deixando a sintaxe de uso da classe MemoryPolynomial mais clara. A implementação dos modelos ainda é confusa e o corte nos valores iniciais e finais ainda está hardcoded, essas melhorias serão implementadas mais tarde. No geral agora o código funciona com a estrutura desejada.

--- behavioral_models/__init__.py
+++ behavioral_models/__init__.py
@@ -1,4 +1,3 @@
 from behavioral_models.io import SystemData
 from behavioral_models.model_parameters import ModelParameters
-from behavioral_models.mem_pol import ComplexMatrix, RealMatrix, LUT, RealMatrixCoefsImag
 from behavioral_models.memory_polynomial import MemoryPolynomial, ComplexMatrix, RealMatrix, LookUpTable
